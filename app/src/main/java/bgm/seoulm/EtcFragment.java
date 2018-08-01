0bb9942dab587b65e588dbe48029fa4afd70a482
BGM <wjdrmf314@gmail.com>
2018-08-01 14:59:22 +0900

    fist commit

---
+++ app/src/main/java/bgm/seoulm/EtcFragment.java
@@ -0,0 +1,13 @@
+package bgm.seoulm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EtcFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.etcfragment,container,false);
+        return view;}
+}
